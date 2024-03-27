73dcc67c9e717a9e990b326afd7a64c227cc7d2c
TK <leandrotk100@gmail.com>
2024-03-27 11:36:17 -0300

    *

--- react/perf-playground/src/App.js
+++ react/perf-playground/src/App.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
+const Child = () => {
+  return <div style={{ border: "1px solid" }}>child</div>;
+};
+
+const Parent = ({ children }) => {
+  const [state, setState] = useState(true);
+  return (
+    <div style={{ padding: "8px" }}>
+      {children}
+      <button
+        onClick={() => {
+          setState(!state);
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+};
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Parent>
+        <Child />
+      </Parent>
+    </div>
+  );
 };
 
 export default App;
