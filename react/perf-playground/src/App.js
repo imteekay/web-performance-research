ba77b4ad23f39d2f7924f21cf289dc62e34499a3
TK <leandrotk100@gmail.com>
2024-03-27 11:00:01 -0300

    *

--- react/perf-playground/src/App.js
+++ react/perf-playground/src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 function App() {
   return <div className="App"></div>;
 }
--- react/perf-playground/src/index.js
+++ react/perf-playground/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
